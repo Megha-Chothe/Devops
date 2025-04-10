19bd2f11128e129500efe632ef94aece8da4c351
Megha <141348759+Megha-Chothe@users.noreply.github.com>
2025-04-10 17:52:31 +0530

    Create Terraform EC2.tf

---
+++ Terraform EC2.tf
@@ -0,0 +1,12 @@
+rovider "aws" {
+  region = "ap-south-1"  # Mumbai region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-002f6e91abff6eb96"  # Amazon Linux 2 AMI for Mumbai (Check latest AMI before deploying)
+  instance_type = "t2.micro"  # Free-tier eligible instance type
+
+  tags = {
+    Name = "Terraform-EC2-Mumbai"
+  }
+}
